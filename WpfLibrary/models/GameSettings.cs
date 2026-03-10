367e99f986798286971fc65c4c91fcfa1da67af9
Дмитро Виноградов <ipz241_vdv@student.ztu.edu.ua>
2026-03-10 12:59:29 +0200

    add GameSettings model for user preferences

---
+++ WpfLibrary/models/GameSettings.cs
@@ -0,0 +1,31 @@
+﻿namespace WpfLibrary.models
+{
+    public class GameSettings
+    {
+        public DifficultyLevel SelectedDifficulty { get; set; }
+        public int CustomRows { get; set; }
+        public int CustomColumns { get; set; }
+        public int CustomMineCount { get; set; }
+        public string Theme { get; set; }
+        public int CellSize { get; set; }
+
+        public GameSettings()
+        {
+            SelectedDifficulty = DifficultyLevel.Easy;
+            CustomRows = 10;
+            CustomColumns = 10;
+            CustomMineCount = 15;
+            Theme = "Light";
+            CellSize = 32;
+        }
+
+        public Difficulty GetDifficulty() => SelectedDifficulty switch
+        {
+            DifficultyLevel.Easy => Difficulty.Easy,
+            DifficultyLevel.Medium => Difficulty.Medium,
+            DifficultyLevel.Hard => Difficulty.Hard,
+            DifficultyLevel.Custom => Difficulty.CreateCustom(CustomRows, CustomColumns, CustomMineCount),
+            _ => Difficulty.Easy
+        };
+    }
+}
