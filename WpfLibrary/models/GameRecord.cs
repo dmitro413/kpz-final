b0d1bc04e96a0ae76b3b703df3ce076db17ffa71
Дмитро Виноградов <ipz241_vdv@student.ztu.edu.ua>
2026-03-10 13:00:23 +0200

    add GameRecord model for leaderboard entries

---
+++ WpfLibrary/models/GameRecord.cs
@@ -0,0 +1,23 @@
+﻿namespace WpfLibrary.models
+{
+    public class GameRecord
+    {
+        public string PlayerName { get; set; }
+        public int TimeSeconds { get; set; }
+        public DifficultyLevel Difficulty { get; set; }
+        public DateTime Date { get; set; }
+
+        public string FormattedTime => $"{TimeSeconds / 60:D2}:{TimeSeconds % 60:D2}";
+        public string FormattedDate => Date.ToString("yyyy-MM-dd HH:mm");
+
+        public GameRecord() { }
+
+        public GameRecord(string playerName, int timeSeconds, DifficultyLevel difficulty)
+        {
+            PlayerName = playerName;
+            TimeSeconds = timeSeconds;
+            Difficulty = difficulty;
+            Date = DateTime.Now;
+        }
+    }
+}
