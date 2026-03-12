10a24192c49f4f84fc2bbbf485a72dfd84467eaf
Дмитро Виноградов <ipz241_vdv@student.ztu.edu.ua>
2026-03-12 20:55:13 +0200

    return non-generic RelayCommand

--- WpfLibrary/Commands/RelayCommand.cs
+++ WpfLibrary/Commands/RelayCommand.cs
@@ -20,4 +20,24 @@ namespace WpfLibrary.Commands
         public bool CanExecute(object? parameter) => _canExecute?.Invoke((T?)parameter) ?? true;
         public void Execute(object? parameter) => _execute((T?)parameter);
     }
+    public class RelayCommand : ICommand
+    {
+        private readonly Action _execute;
+        private readonly Func<bool>? _canExecute;
+
+        public event EventHandler? CanExecuteChanged
+        {
+            add => CommandManager.RequerySuggested += value;
+            remove => CommandManager.RequerySuggested -= value;
+        }
+
+        public RelayCommand(Action execute, Func<bool>? canExecute = null)
+        {
+            _execute = execute ?? throw new ArgumentNullException(nameof(execute));
+            _canExecute = canExecute;
+        }
+
+        public bool CanExecute(object? parameter) => _canExecute?.Invoke() ?? true;
+        public void Execute(object? parameter) => _execute();
+    }
 }
