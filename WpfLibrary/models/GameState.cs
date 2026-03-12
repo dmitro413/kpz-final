32e5d80098a211a2942bff06e94e62c1ce7871a5
Дмитро Виноградов <ipz241_vdv@student.ztu.edu.ua>
2026-03-12 17:37:36 +0200

    make a new gamestate

--- WpfLibrary/models/GameState.cs
+++ WpfLibrary/models/GameState.cs
@@ -1,12 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace WpfLibrary.models
+﻿namespace WpfLibrary.models
 {
-    internal class GameState
+    public enum GamePhase
     {
+        NotStarted,
+        InProgress,
+        Won,
+        Lost
+    }
+
+    public class GameState
+    {
+        public GamePhase Phase { get; }
+
+        public bool IsActive => Phase == GamePhase.InProgress;
+        public bool IsOver => Phase == GamePhase.Won || Phase == GamePhase.Lost;
+        public bool IsWon => Phase == GamePhase.Won;
+
+        private GameState(GamePhase phase)
+        {
+            Phase = phase;
+        }
+
+        public static GameState NotStarted => new GameState(GamePhase.NotStarted);
+        public static GameState InProgress => new GameState(GamePhase.InProgress);
+        public static GameState Won => new GameState(GamePhase.Won);
+        public static GameState Lost => new GameState(GamePhase.Lost);
     }
 }
