9b26a2fb123a3d47a6014c67fbf5b34e87471cfe
Дмитро Виноградов <ipz241_vdv@student.ztu.edu.ua>
2026-03-18 14:03:21 +0200

    add all UserControl views and code-behind files

--- KPZ-FINAL/Views/GameHistoryView.xaml.cs
+++ KPZ-FINAL/Views/GameHistoryView.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System.Text;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -9,14 +13,14 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace KPZ_FINAL
+namespace KPZ_FINAL.Views
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for GameHistoryView.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class GameHistoryView : UserControl
     {
-        public MainWindow()
+        public GameHistoryView()
         {
             InitializeComponent();
         }
